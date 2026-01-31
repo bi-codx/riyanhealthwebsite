7f891b26775e99a174ed161c31f32961e539eb06
Fadillilah Alhassan <alhassanfadillilah70@gmail.com>
2026-01-31 11:29:01 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Simplifies the import paths and removes redundant versioned aliases
       '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
     target: 'esnext',
-    outDir: 'dist', // Vercel standard for Vite is 'dist'
-    sourcemap: false,
-  },
-  server: {
-    port: 3000,
+    outDir: 'dist', // Standardize to 'dist' for Vercel
   },
 });
